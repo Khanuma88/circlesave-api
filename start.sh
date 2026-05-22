e5cbe3d24cd62c3c12de93b70d9a33d6530720e2
Ханума <khanuma.zamirova@narxoz.kz>
2026-05-22 12:53:04 +0500

    update

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 node node_modules/prisma/build/index.js migrate deploy
+node -e "
+const {PrismaClient} = require('@prisma/client');
+const p = new PrismaClient();
+p.user.updateMany({where:{phone:'+77011111111'},data:{role:'ADMIN',verifiedEmail:true}})
+.then(()=>p.user.updateMany({where:{phone:'+77077777771'},data:{role:'ORGANIZER',verifiedEmail:true}}))
+.then(()=>p.\$disconnect())
+.catch(console.error)
+" 
 node src/server.js
